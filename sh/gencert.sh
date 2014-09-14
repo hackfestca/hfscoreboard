6763642e483cbbbf710c41076d81f9983802b4d3
Mart <martin@md-desktop.lsec.net>
2014-09-13 23:37:28 -0400

    Updated gencert.sh and generated new certificates for SSL

--- sh/gencert.sh
+++ sh/gencert.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-openssl req -x509 -newkey rsa:2048 -keyout certs/key.pem -nodes -out certs/cert.pem -days 5
+# Gen cert
+openssl req -sha256 -out scoreboard-web-ssl.csr -new -newkey rsa:2048 -nodes -keyout scoreboard-web-ssl.key -days 365
 
+# Self sign
+openssl x509 -req -days 365 -in scoreboard-web-ssl.csr -signkey scoreboard-web-ssl.key -out scoreboard-web-ssl.crt
+
+# For postgresql
 #openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout postgres-server.key -out postgres-server.crt
