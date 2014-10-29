25d8d9538dfa2ebd024b4bffb12864ad3fe5ee24
ek0 <mail.jessy.campos@gmail.com>
2014-10-29 17:17:18 -0400

    Remove a debug print

--- scoreboard.py
+++ scoreboard.py
@@ -200,7 +200,6 @@ class ChallengesHandler(BaseHandler):
     def get(self):
         try:
             categories = self.client.getCatProgressFromIp(self.request.remote_ip)
-            print(categories)
             challenges = self.client.getFlagProgressFromIp(self.request.remote_ip)
         except PLPGSQLRaiseError as e:
             message = e.message
