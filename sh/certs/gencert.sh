b2c68ab34e35048b70ff70ac537b2aa061d7601a
Martin (pwnbox) <martin.dube@gmail.com>
2014-12-28 00:14:17 -0500

    Updated certs

---
+++ sh/certs/gencert.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+ROOT='.'
+CA_NAME='sb-ca'                 # Should fit with openssl.cnf ca name (line 2)
+CONFIG_FILE=$ROOT'/openssl.cnf'
+CA_FILE_PATH=$ROOT'/'$CA_NAME
+CA_KEY_FILE=$CA_FILE_PATH'.key'
+CA_CRT_FILE=$CA_FILE_PATH'.crt'
+CA_SUBJECT="/C=CA/ST=Qc/L=Quebec/O=My Org/OU=My Org Department/CN=My Org CA"
+DB_FILE_PATH=$ROOT'/srv.psql.scoreboard.db'
+DB_KEY_FILE=$DB_FILE_PATH'.key'
+DB_CSR_FILE=$DB_FILE_PATH'.csr'
+DB_CRT_FILE=$DB_FILE_PATH'.crt'
+DB_SUBJECT="/C=CA/ST=Qc/L=Quebec/O=My Org/OU=My Org Department/CN=db.hf"
+CLI_NAMES=('player' 'flagupdater' 'web' 'hfowner')  # Must match with database users
+
+mkdir -p $CA_FILE_PATH
+touch $ROOT'/'$CA_NAME'.db'
+touch $ROOT'/'$CA_NAME'.crl.srl'
+echo "21" > $ROOT'/'$CA_NAME'.crt.srl'
+echo "" >> $ROOT'/'$CA_NAME'.crt.srl'
+rm $CA_CRT_FILE
+rm $CA_KEY_FILE
+
+# Generate CA and self-sign
+openssl req -new -x509 -nodes \
+    -config $CONFIG_FILE \
+    -keyout $CA_KEY_FILE \
+    -out $CA_CRT_FILE \
+    -subj "$CA_SUBJECT" \
+    -extensions ca_ext
+openssl x509 -in $CA_CRT_FILE -text
+
+# Generate and sign DB server cert
+openssl req -new -nodes \
+    -config $CONFIG_FILE \
+    -keyout $DB_KEY_FILE \
+    -out $DB_CSR_FILE \
+    -subj "$DB_SUBJECT"
+openssl ca -batch \
+    -config $CONFIG_FILE \
+    -in $DB_CSR_FILE \
+    -out $DB_CRT_FILE \
+    -extensions server_reqext
+openssl x509 -in $DB_CRT_FILE -text
+
+# Generate and sign client certs
+for name in "${CLI_NAMES[@]}"
+do
+    FILE_PATH=$ROOT'/'$name
+    KEY_FILE=$FILE_PATH'.key'
+    CSR_FILE=$FILE_PATH'.csr'
+    CRT_FILE=$FILE_PATH'.crt'
+
+    openssl req -new -nodes \
+        -config $CONFIG_FILE \
+        -keyout $KEY_FILE \
+        -out $CSR_FILE \
+        -subj "/C=CA/ST=Qc/L=Quebec/O=My Org/OU=My Org Department/CN=$name" 
+    openssl ca -batch \
+        -config $CONFIG_FILE \
+        -in $CSR_FILE \
+        -out $CRT_FILE \
+        -extensions client_reqext
+    openssl x509 -in $CRT_FILE -text
+
+    # tmp
+    mv $KEY_FILE 'cli.psql.scoreboard.'$name'.key'
+    mv $CSR_FILE 'cli.psql.scoreboard.'$name'.csr'
+    mv $CRT_FILE 'cli.psql.scoreboard.'$name'.crt'
+done
--- sh/gencert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Self signed web scoreboard certificate
-#
-#openssl req -sha256 -out scoreboard-web-ssl.csr -new -newkey rsa:2048 -nodes -keyout scoreboard-web-ssl.key -days 365
-#openssl x509 -req -days 365 -in scoreboard-web-ssl.csr -signkey scoreboard-web-ssl.key -out scoreboard-web-ssl.crt
-
-# For postgresql
-#openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout postgres-server.key -out postgres-server.crt
