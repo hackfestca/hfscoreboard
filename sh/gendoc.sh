834970352e2d8255b56bc949195c16c215821d3f
Martin Dubé <mdube@gosecure.ca>
2015-12-13 21:55:30 -0500

    Updated docs and gendoc.sh script

--- sh/gendoc.sh
+++ sh/gendoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ROOT_FOLDER='../'
-/usr/bin/epydoc -v --graph=all -o $ROOT_FOLDER''docs --html $ROOT_FOLDER''lib/*.py 
+python2.7 /usr/local/bin/epydoc -v --graph=all -o $ROOT_FOLDER''docs --html $ROOT_FOLDER''lib/*.py 
