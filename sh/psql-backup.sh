32b4f117548914e157bd7a527f06ea765f0500d6
Martin Dubé <martin.dube@gmail.com>
2017-11-06 14:39:31 -0500

    Updated backup script

--- sh/psql-backup.sh
+++ sh/psql-backup.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-pg_dump -a --compress=9 -f ../backups/scoreboard-$(date '+%Y-%m-%d-%H%M%S').sql.gz "sslmode=require host=db.hf dbname=scoreboard user=owner sslcert=../certs/hf.cli.db.owner.crt sslkey=../certs/hf.cli.db.owner.key"
+
+# Old way
+#pg_dump -a --compress=9 -f ../backups/scoreboard-$(date '+%Y-%m-%d-%H%M%S').sql.gz "sslmode=require host=db.hf dbname=scoreboard user=owner sslcert=../certs/hf.cli.db.owner.crt sslkey=../certs/hf.cli.db.owner.key"
+
+# HF2k17 way
+pg_dump -a --compress=9 -f ./backups/scoreboard-$(date '+%Y-%m-%d-%H%M%S').sql.gz scoreboard
